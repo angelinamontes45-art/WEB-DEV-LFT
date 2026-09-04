3816989858a9d93f72d964c61006de95d2d55961
Montes Angelina <angelinamontes45@gmail.com>
2026-09-05 03:50:43 +0800

    Load shared portal responsive styles

--- Includes/header.php
+++ Includes/header.php
@@ -10,6 +10,7 @@ $loggedInUser = currentUser();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle ?? 'LFT Dumaguete') ?></title>
     <link rel="stylesheet" href="<?= $basePath ?>assets/css/style.css">
+    <link rel="stylesheet" href="<?= $basePath ?>assets/css/portal.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
 </head>
 <body>
